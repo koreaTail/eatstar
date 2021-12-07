e4983fe4ef27e484f2012c8e82285ffa7dbb65aa
leesangjun <prokjun@gmail.com>
2021-12-07 11:30:23 +0900

    #3.0 Your First Server (12:03)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 
 import express from "express";
 // const express = require("express");
+const PORT = 4000;
 const app = express();
 
-const handelListening = () => console.log("Server listenting on port 4000 🚀")
+const handelListening = () => console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
 
 app.listen(4000, handelListening);
