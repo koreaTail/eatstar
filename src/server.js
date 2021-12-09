26f7c105e9c0e0a3b40ae6aff8773b334cbbdb51
leesangjun <prokjun@gmail.com>
2021-12-09 14:47:10 +0900

    #3.1 GET Requests (05:05)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
-
+// 서버를 만들고
 import express from "express";
 // const express = require("express");
 const PORT = 4000;
 const app = express();
 
-const handelListening = () => console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
+
+// 햄버거처럼.. 서버의 내용물을 채운다.
+app.get("/", () => console.log("Somebody is trying to go home"));
+
+
+
+// 서버를 듣고
+const handelListening = (req, res) => console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
 
 app.listen(4000, handelListening);
