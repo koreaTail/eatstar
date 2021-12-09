c4aeea2d9589b35a203671646df819604dbd6209
leesangjun <prokjun@gmail.com>
2021-12-09 15:06:56 +0900

    #3.3 Responses (11:23)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-// 서버를 만들고
+
 import express from "express";
 // const express = require("express");
 const PORT = 4000;
+
+
+// 서버를 만들고
 const app = express();
 
 
 // 햄버거처럼.. 서버의 내용물을 채운다.
-app.get("/", () => console.log("Somebody is trying to go home"));
+const handleHome = (req, res) => {
+    console.log(req);
+}
+
+app.get("/", handleHome)
 
 
 
