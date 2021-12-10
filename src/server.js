efe10c825064ea02ed4ae0a4b92e1dc7a8103861
leesangjun <prokjun@gmail.com>
2021-12-10 14:52:44 +0900

    #3.6 Middlewares part Two (11:40)

--- src/server.js
+++ src/server.js
@@ -7,11 +7,19 @@ const PORT = 4000;
 // 서버를 만들고
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log("Im in the middle!");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
 }
 
+const privateMiddleware =(req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("통과되신 분이시군요. 지나가세요.")
+    next();
+}
 
 // 햄버거처럼.. 서버의 내용물을 채운다.
 const handleHome = (req, res, next) => {
@@ -22,11 +30,18 @@ const handleLogin = (req,res) => {
     return res.send("Login here")
 }
 
-app.get("/", gossipMiddleware, handleHome)
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge")
+}
+   
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected)
 
 
 // 서버를 듣고
 const handelListening = (req, res) => console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
 
-app.listen(4000, handelListening);
+app.listen(4000, handelListening); 
