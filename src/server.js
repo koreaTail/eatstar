868a7304f79bade366df0c405a1d96da068fdede
leesangjun <prokjun@gmail.com>
2021-12-10 14:57:13 +0900

    #3.6(9) 코드 정리 후 마무리

--- src/server.js
+++ src/server.js
@@ -12,33 +12,12 @@ const logger = (req, res, next) => {
     next();
 }
 
-const privateMiddleware =(req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("통과되신 분이시군요. 지나가세요.")
-    next();
-}
-
 // 햄버거처럼.. 서버의 내용물을 채운다.
 const handleHome = (req, res, next) => {
     return res.end();
 }
-
-const handleLogin = (req,res) => {
-    return res.send("Login here")
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge")
-}
    
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected)
+app.get("/", logger, handleHome);
 
 
 // 서버를 듣고
