c4d524da74535fa043847712c187ba369bb6a582
leesangjun <prokjun@gmail.com>
2021-12-15 11:34:45 +0900

    #4.0 What are Routers? (07:37)

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 
 import express from "express";
+import morgan from "morgan";
+
 // const express = require("express");
 const PORT = 4000;
 
 
 // 서버를 만들고
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
+const logger = morgan("dev")
 
 // 햄버거처럼.. 서버의 내용물을 채운다.
 const handleHome = (req, res, next) => {
     return res.end();
 }
    
-app.get("/", logger, handleHome);
+app.use(logger)
+app.get("/", handleHome);
 
 
 // 서버를 듣고
